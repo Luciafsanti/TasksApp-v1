740b07ec794865030c3795eaa6e2b8bae7279fcc
Luciafsanti <144744219+Luciafsanti@users.noreply.github.com>
2023-10-21 21:07:58 -0300

    Update filter.js

--- comandos/filter.js
+++ comandos/filter.js
@@ -2,7 +2,6 @@ const titles = require("./list");
 
 const filter = (word) => {
     let result = []
-    arrayWord = word.split();
     for (let i=0; i<titles.length; i++){
         if (titles[i].includes(word)){
             result[i] = titles[i];
